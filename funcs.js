1566e918f87466149d48e8d702efc9fd8d5d351b
webeato <webeato.ad@gmail.com>
2026-05-21 16:31:06 +0330

    modular development

---
+++ funcs.js
@@ -0,0 +1,15 @@
+exports.add = function add(num1, num2) {
+    return num1 + num2
+}
+
+exports.minus = function minus(num1, num2) {
+    return num1 - num2
+}
+
+exports.devide = function devide(num1, num2) {
+    return num1 / num2
+}
+
+exports.power = function power(num1, num2) {
+    return num1 ** num2
+}
---
+++ server.js
@@ -0,0 +1,9 @@
+const calc = require('./funcs')
+
+console.log('webeato.ir');
+
+console.log(calc);
+console.log(calc.add(12, 2));
+console.log(calc.minus(12, 2));
+console.log(calc.devide(12, 2));
+console.log(calc.power(12, 2));
