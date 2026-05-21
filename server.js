e55c97d78763937c54662301d0df5892e870da71
webeato <webeato.ad@gmail.com>
2026-05-21 17:22:08 +0330

    Working with the fs module - Part 3

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
 const calc = require('./funcs')
 const fs = require('fs') // File System
 
-// fs.writeFile('./data.txt', 'Webeato Is The Best.', err => {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log('File is Created !!!');
-// })
 
-// fs.rename('./info.txt', './my-data.txt', err => {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log('File is renamed !!!');
-// })
+fs.readFile('./my-data.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
 
-let courses = [
-    { id: 1, title: 'React', price: 2_200_000 },
-    { id: 2, title: 'Nodejs', price: 1_900_000 },
-    { id: 1, title: 'Js Expert', price: 'Free' },
-]
-
-courses.forEach(course => {
-    fs.appendFile('./my-data.txt', `\n${course.title} - ${course.price}`, err => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('Text is appended !!!');
-    })
 })
