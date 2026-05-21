fe23c6e07ed532c18dc39334d711dc4df04993ed
webeato <webeato.ad@gmail.com>
2026-05-21 17:17:05 +0330

    Working with the fs module - Part 2

--- server.js
+++ server.js
@@ -9,10 +9,26 @@ const fs = require('fs') // File System
 //     console.log('File is Created !!!');
 // })
 
-fs.rename('./data.txt', './info.txt', err => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('File is renamed !!!');
+// fs.rename('./info.txt', './my-data.txt', err => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log('File is renamed !!!');
+// })
+
+let courses = [
+    { id: 1, title: 'React', price: 2_200_000 },
+    { id: 2, title: 'Nodejs', price: 1_900_000 },
+    { id: 1, title: 'Js Expert', price: 'Free' },
+]
+
+courses.forEach(course => {
+    fs.appendFile('./my-data.txt', `\n${course.title} - ${course.price}`, err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Text is appended !!!');
+    })
 })
