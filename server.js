791154de31c3bb83ee519b7637ae0a2ce8cebd39
webeato <webeato.ad@gmail.com>
2026-05-24 13:02:12 +0330

    Practice Users

--- server.js
+++ server.js
@@ -16,22 +16,20 @@ let database = {
 
 
 const server = http.createServer((req, res) => {
-    const queryStrings = url.parse(req.url, true).query
+    const parsedUrl = url.parse(req.url, true)
 
-    console.log(queryStrings.name);
+    if (parsedUrl.pathname === '/api/users') {
+        const filtredUser = database.users.filter(user => user.id === Number(parsedUrl.query.id))
 
-    if (req.url === '/api/users') {
-        let users = database.users;
-        res.write(JSON.stringify(users))
-        res.end()
-    } else if (req.url === '/api/courses') {
-        let courses = database.courses;
-        res.write(JSON.stringify(courses))
-        res.end()
-    } else {
-        res.write('Error 404 :/ Api not found')
-        res.end()
+        if (filtredUser.length) {
+            res.write(JSON.stringify(filtredUser))
+            res.end()
+        } else {
+            res.write('user not found')
+            res.end()
+        }
     }
+
 })
 
 server.listen(3000)
