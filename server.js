9913f0d9475b7d7b57f0cea699e3b9d03b565fd6
webeato <webeato.ad@gmail.com>
2026-05-24 11:34:23 +0330

    Mini Project Users Info

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const http = require('http')
 
-// http.createServer((req, res) => {
-//     console.log('Request:', req.method, req.url);
-// }).listen(3000)
+let database = {
+    users: [
+        { id: 1, username: 'Ali', email: 'ali@gmail.com', password: '12345' },
+        { id: 2, username: 'Mohammad', email: 'mohammad@gmail.com', password: 'mmd_345' },
+        { id: 3, username: 'Amin', email: 'amin@gmail.com', password: 'amn132' },
+    ],
+    courses: [
+        { id: 1, title: 'Api Dev With Nodejs', price: 237_000 },
+        { id: 2, title: 'React Course', price: 2_200_000 },
+        { id: 3, title: 'Js Expert', price: 'Free' },
+    ]
+}
 
 
 const server = http.createServer((req, res) => {
-    console.log('Request:', req.method, req.url);
+    console.log(req.url);
+
+    if (req.url === '/api/users') {
+        let users = database.users;
+        res.write(JSON.stringify(users))
+        res.end()
+    } else if (req.url === '/api/courses') {
+        let courses = database.courses;
+        res.write(JSON.stringify(courses))
+        res.end()
+    } else {
+        res.write('Error 404 :/ Api not found')
+        res.end()
+    }
 })
 
 server.listen(3000)
