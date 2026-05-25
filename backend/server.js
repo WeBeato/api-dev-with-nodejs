765a68ac41ab58b4b50865f5ad1e9310b4374fc1
webeato <webeato.ad@gmail.com>
2026-05-25 10:54:15 +0330

    Backend coding of the login form project

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,32 @@
-const http = require('http')
+const http = require('http');
+const url = require('url');
 
-let database = {}
+let database = {
+    users: [
+        { id: 1, username: 'ali', password: '90ali95', age: 16 },
+        { id: 2, username: 'amin', password: '678', age: 22 },
+        { id: 3, username: 'q_yolme', password: 'sfg54', age: 20 },
+        { id: 4, username: 'mohammad', password: '007', age: 18 },
+        { id: 5, username: 'babak', password: 'asgnivrsrj321', age: 21 },
+    ]
+};
 
 
-const server = http.createServer((req, res) => { })
+const server = http.createServer((req, res) => {
+    const urlParams = url.parse(req.url, true);
 
-// server.listen()
+    if (urlParams.pathname === '/api/users') {
+        const mainUser = database.users.filter(user => user.username.toLowerCase() === urlParams.query.username.toLowerCase());
+
+        if (mainUser.length) {
+            res.write(JSON.stringify(mainUser[0]));
+            res.end();
+        } else {
+            res.write(JSON.stringify(null));
+            res.end();
+        }
+    }
+});
+
+
+server.listen(3000);
