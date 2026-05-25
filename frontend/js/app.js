c521d972aa7c753adf597fbdd9b1481da60bd443
webeato <webeato.ad@gmail.com>
2026-05-25 15:29:52 +0330

    Data Validation

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,16 +1,22 @@
 const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
 const form = document.querySelector('form');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    let apiUrl = `http://localhost:3000/api/users?username=${usernameInput.value}`;
-    fetch(apiUrl).then(res => res.json()).then(data => {
-        console.log(data)
-        if (data) {
-            alert('You can go to your user panel :))');
-        }
-        else {
-            alert('You must first register!')
-        }
-    });
+    if (usernameInput.value.length >= 3 && passwordInput.value.length >= 8) {
+        let apiUrl = `http://localhost:3000/api/users?username=${usernameInput.value}`;
+        fetch(apiUrl).then(res => res.json()).then(data => {
+            console.log(data)
+            if (data) {
+                alert('You can go to your user panel :))');
+            }
+            else {
+                alert('You must first register!');
+            }
+        });
+    } else {
+        alert('Inputs values are invalid!!');
+    }
+
 });
