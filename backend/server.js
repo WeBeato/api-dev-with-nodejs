8dd33ed90947bcfe2bb92337de0ed126c465e968
webeato <webeato.ad@gmail.com>
2026-05-27 11:37:21 +0330

    Get body from Request in Api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const http = require('http')
 const express = require('express')
+const { json } = require('stream/consumers')
 
 const app = express()
+
+app.use(express.json({ extended: false }))
 
 // const server = http.createServer((req, res) => {
 //     if (req.method === 'GET' && req.url === '/api/products') {
@@ -13,6 +16,13 @@ const app = express()
 // server.listen(3000, () => {
 //     console.log('Server Run on Port 3000');
 // })
+
+let db = {
+    users: [
+        { id: 1, name: 'Ali', phone: '09123456789' },
+        { id: 2, name: 'Mohammd', phone: '09999999999' },
+    ]
+}
 
 app.get('/api/products', (req, res) => {
     res.send('Yooo!!')
@@ -31,6 +41,13 @@ app.get('/users/:userID/articles/:articleID', (req, res) => {
 app.get('/products/:productName', (req, res) => {
     console.log(req.params);
     res.send(`Course Name: ${req.params.productName}`)
+})
+
+app.post('/api/new-user', (req, res) => {
+    console.log(req.body);
+    let newUser = req.body
+    db.users.push(newUser)
+    res.send(`New user created! => ${JSON.stringify(db.users)}`)
 })
 
 app.listen(3000, () => {
