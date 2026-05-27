2b173185e6e47522ce0bd185ab1a419c58de0f17
webeato <webeato.ad@gmail.com>
2026-05-27 11:20:38 +0330

    Getting Request Params in Api with Express

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,18 @@ app.get('/api/products', (req, res) => {
 app.get('/api/admins', (req, res) => {
     res.send('Admins Show')
 })
+app.delete('/api/user/remove/:userid', (req, res) => {
+    console.log(req.params);
+    res.send('User Deleted')
+})
+app.get('/users/:userID/articles/:articleID', (req, res) => {
+    console.log(req.params);
+    res.send(`Article ID: ${req.params.articleID} && User ID: ${req.params.userID}`)
+})
+app.get('/products/:productName', (req, res) => {
+    console.log(req.params);
+    res.send(`Course Name: ${req.params.productName}`)
+})
 
 app.listen(3000, () => {
     console.log('Server Run on Port 3000');
