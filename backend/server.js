c29c2701b89d8fb9b66fe1628b28d9b8f703f203
webeato <webeato.ad@gmail.com>
2026-05-27 11:03:20 +0330

    Building Api with Express

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,26 @@
-const http = require('http');
-const url = require('url');
+const http = require('http')
+const express = require('express')
 
-let database = {
-    users: [
-        { id: 1, username: 'ali', password: '90ali95', age: 16 },
-        { id: 2, username: 'amin', password: '678', age: 22 },
-        { id: 3, username: 'q_yolme', password: 'sfg54', age: 20 },
-        { id: 4, username: 'mohammad', password: '007', age: 18 },
-        { id: 5, username: 'babak', password: 'asgnivrsrj321', age: 21 },
-    ]
-};
+const app = express()
 
+// const server = http.createServer((req, res) => {
+//     if (req.method === 'GET' && req.url === '/api/products') {
+//         res.write('Products')
+//         res.end()
+//     }
+// })
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+// server.listen(3000, () => {
+//     console.log('Server Run on Port 3000');
+// })
 
-    const urlParams = url.parse(req.url, true);
+app.get('/api/products', (req, res) => {
+    res.send('Yooo!!')
+})
+app.get('/api/admins', (req, res) => {
+    res.send('Admins Show')
+})
 
-    if (urlParams.pathname === '/api/users') {
-        const mainUser = database.users.filter(user => user.username.toLowerCase() === urlParams.query.username.toLowerCase());
-
-        if (mainUser.length) {
-            res.write(JSON.stringify(mainUser[0]));
-            res.end();
-        } else {
-            res.write(JSON.stringify(null));
-            res.end();
-        }
-    }
-});
-
-
-server.listen(3000);
+app.listen(3000, () => {
+    console.log('Server Run on Port 3000');
+})
