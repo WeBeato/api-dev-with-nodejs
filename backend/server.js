1330b7c34d51b477ab19769f27f4d75be6504307
webeato <webeato.ad@gmail.com>
2026-05-28 12:01:23 +0330

    Implementing APIs for the Registration Form Project

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -6,58 +5,16 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-
-// app.use(express.json({ extended: false }))
 app.use(bodyParser.json())
 
-// const server = http.createServer((req, res) => {
-//     if (req.method === 'GET' && req.url === '/api/products') {
-//         res.write('Products')
-//         res.end()
-//     }
-// })
-
-// server.listen(3000, () => {
-//     console.log('Server Run on Port 3000');
-// })
-
-let db = {
+const db = {
     users: [
-        { id: 1, name: 'Ali', phone: '09123456789' },
-        { id: 2, name: 'Mohammd', phone: '09999999999' },
+        { id: 1, info: 'mohammad ghanavati', username: 'Mohammad', password: '1290' },
+        { id: 2, info: 'amin mehrabi', username: 'Amin', password: '9876' },
+        { id: 3, info: 'sajad mosavi', username: 'Sajad', password: 'ssd2000' }
     ]
 }
 
-app.get('/api/products', (req, res) => {
-    res.send('Yooo!!')
-})
-app.get('/api/admins', (req, res) => {
-    res.send('Admins Show')
-})
-app.delete('/api/user/remove/:userid', (req, res) => {
-    console.log(req.params);
-    res.send('User Deleted')
-})
-app.get('/users/:userID/articles/:articleID', (req, res) => {
-    console.log(req.params);
-    res.send(`Article ID: ${req.params.articleID} && User ID: ${req.params.userID}`)
-})
-app.get('/products/:productName', (req, res) => {
-    console.log(req.params);
-    res.send(`Course Name: ${req.params.productName}`)
-})
-
 app.post('/api/new-user', (req, res) => {
     console.log(req.body);
-    let newUser = req.body
-    db.users.push(newUser)
-    res.send(`New user created! => ${JSON.stringify(db.users)}`)
-})
-
-app.get('/api/teachers', cors(), (req, res) => {
-    res.send('Show Teachers');
-})
-
-app.listen(3000, () => {
-    console.log('Server Run on Port 3000');
 })
