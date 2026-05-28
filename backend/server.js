9f9cce9809891145864accc705d3d0428f954266
webeato <webeato.ad@gmail.com>
2026-05-28 12:50:41 +0330

    Working with Router in Express and Categorizing Routes

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const usersRoutes = require('./routes/usersRoutes')
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 
-const db = {
-    users: [
-        { id: 1, info: 'mohammad ghanavati', username: 'Mohammad', password: '1290' },
-        { id: 2, info: 'amin mehrabi', username: 'Amin', password: '9876' },
-        { id: 3, info: 'sajad mosavi', username: 'Sajad', password: 'ssd2000' }
-    ]
-}
-
-app.post('/api/new-user', (req, res) => {
-    console.log(req.body);
-    let newUser = { id: db.users.length + 1, ...req.body }
-    let allUsers = [...db.users]
-    allUsers.push(newUser)
-    console.log(`Users => ${JSON.stringify(allUsers)}`);
-    res.send('New User Created :))')
-})
+app.use('/api/users',usersRoutes)
+// app.use('/api/articles',articlesRoutes)
+// app.use('/api/products',productsRoutes)
 
 app.listen(3000)
